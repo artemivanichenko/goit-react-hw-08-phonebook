38fe9451b272630f080d44dce9406e2b974a1ba4
Artem <ivanichenko.ua@gmail.com>
2023-04-19 02:06:28 +0300

    completed PhoneBook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
 import { PhoneBook } from './PhoneBook/PhoneBook';
 
 export const App = () => {
-  const getData = data => {
-    console.log(data);
-  };
-  return <PhoneBook getData={getData} />;
+  return <PhoneBook />;
 };
