7931cba982d5649f4ba19d348a497cffb694a052
Artem <ivanichenko.ua@gmail.com>
2023-04-23 17:11:56 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,6 +55,19 @@ export class App extends Component {
     return contacts;
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const contacts = this.filterContacts();
