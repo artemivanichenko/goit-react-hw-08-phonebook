315d241b9508953a8c6baedc9fc1d895058b9251
Artem <ivanichenko.ua@gmail.com>
2023-04-25 18:33:16 +0300

    add index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
-import { ContactsList } from 'components/ContactsList/ContactsList';
-import { Filter } from 'components/Filter/Filter';
-import { nanoid } from 'nanoid';
 import { Component } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Section } from './Section/Section';
+import { nanoid } from 'nanoid';
+import { contacts } from 'data/contacts';
+import { Section, ContactForm, Filter, ContactsList } from './index';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'David Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: contacts,
     filter: '',
   };
 
---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { ContactsList } from './ContactsList/ContactsList';
+export { Filter } from './Filter/Filter';
+export { ContactForm } from './ContactForm/ContactForm';
+export { Section } from './Section/Section';
